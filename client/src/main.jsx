3d673e7c0981e9a4ee9621b6c2c545be831c194d
Michael Sweeney-Glover <sweeneyuk@gmail.com>
2026-07-05 18:39:52 +0100

    Add SW unregister script to force cleanup of old service workers intercepting the API

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,6 +3,15 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+// Force unregister all service workers to prevent stale intercepts
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.getRegistrations().then((registrations) => {
+    for (let registration of registrations) {
+      registration.unregister();
+    }
+  });
+}
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
